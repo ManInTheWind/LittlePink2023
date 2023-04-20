b11389c1b24aba749022bcf6f35a0f0ad48fd4c2
KangkangInfinite <32859126+ManInTheWind@users.noreply.github.com>
2023-04-20 11:57:05 +0800

    Initial Commit

---
+++ LittlePink2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink2023
+//
+//  Created by kangkang on 2023/4/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
